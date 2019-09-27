723ca04f22a1ba838a727db78632bd5cccc7d4a7
alangogoro <cayne9160@gmail.com>
2019-09-27 18:26:43 +0800

    Common.java new changed.

--- app/src/main/java/com/gt/running_client/Common.java
+++ app/src/main/java/com/gt/running_client/Common.java
@@ -7,6 +7,6 @@ public class Common {
 
     /* Toast */
     public static void toast(Context context, String message){
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     }
 }
