989cb54e6bfbd360cceea9371ec2936f7160231c
alangogoro <cayne9160@gmail.com>
2019-09-27 18:20:16 +0800

    Added Common.java for testing branch

---
+++ app/src/main/java/com/gt/running_client/Common.java
@@ -0,0 +1,12 @@
+package com.gt.running_client;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Common {
+
+    /* Toast */
+    public static void toast(Context context, String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
