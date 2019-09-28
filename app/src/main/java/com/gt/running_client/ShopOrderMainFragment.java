034e57d960f54308d3b40f27d8d213ac95d3ecbf
k20057 <k20057@yahoo.com.tw>
2019-09-28 20:00:47 +0800

    ---

--- app/src/main/java/com/gt/running_client/ShopOrderMainFragment.java
+++ app/src/main/java/com/gt/running_client/ShopOrderMainFragment.java
@@ -1,21 +1,51 @@
 package com.gt.running_client;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ShopOrderMainFragment extends Fragment {
+
+    private Activity activity;
+    private Button btConfirm;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = getActivity();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        super.onCreateView(inflater, container, savedInstanceState);
+        activity.setTitle("輸入資訊");
         return inflater.inflate(R.layout.fragment_shop_order_main, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btConfirm = view.findViewById(R.id.btConfirm);
+
+        btConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view)
+                        .navigate(R.id.action_shopOrderMainFragment_to_run2Fragment);
+            }
+        });
+
+    }
 }
---
+++ app/src/main/java/com/gt/running_client/run2Fragment.java
@@ -0,0 +1,31 @@
+package com.gt.running_client;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class run2Fragment extends Fragment {
+
+
+    public run2Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_run2, container, false);
+    }
+
+}
