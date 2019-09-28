a6d8850a6d116d2ea04bc7f4e0be693d26a88733
k20057 <k20057@yahoo.com.tw>
2019-09-28 12:42:06 +0800

    ---

--- app/src/main/java/com/gt/running_client/ShopOrderMainFragment.java
+++ app/src/main/java/com/gt/running_client/ShopOrderMainFragment.java
@@ -9,22 +9,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ShopOrderMainFragment extends Fragment {
-
-
-    public ShopOrderMainFragment() {
-        // Required empty public constructor
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_shop_order_main, container, false);
     }
 
